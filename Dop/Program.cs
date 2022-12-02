5a769dfff41fbadb946ff0706048f4b057055a3b
GLetnikova <g.letnikova@gmail.com>
2022-12-02 13:09:45 +0300

    DZ_2seminar

---
+++ Dop/Program.cs
@@ -0,0 +1,22 @@
+﻿// Второй максимум
+//
+
+Console.Clear();
+
+
+Console.WriteLine("введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int max1 = n;
+int max2 = n;
+while (n != 0)
+{
+    if (n > max1) 
+    { 
+        max2 = max1;
+        max1 = n;
+    }
+        Console.WriteLine("введите число: ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+}
+Console.WriteLine($"второй максимум = {max2}");
---
+++ Task13/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+Console.Clear();
+
+Console.WriteLine("введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N<0) N = (-1)*N;
+
+if (N>-100 && N<100) Console.WriteLine("третьей цифры нет");
+else         Console.WriteLine($"{(N/100)%10}");
