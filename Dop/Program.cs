0908bb8820ab5a0867d03a0cef52dcd70180e740
GLetnikova <g.letnikova@gmail.com>
2022-12-03 10:04:19 +0300

    Доп задание

--- Dop/Program.cs
+++ Dop/Program.cs
@@ -1,26 +1,12 @@
 ﻿// Определить второй максимум. 
-// Входные данные: гарантируется, что будет 0, и перед ним как минимум 2 числа
-//
+// Входные данные: гарантируется, что будет 0
 
 Console.Clear();
-
-/*
-Console.WriteLine("введите число: ");
-int max1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите число: ");
-int max2 = Convert.ToInt32(Console.ReadLine());
-
-if (max2 > max1) { int k = max1; max1 = max2; max2 = k; }
-
-int n = max1;
-*/
-
 
 Console.WriteLine("введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 int max1 = n;
 int max2 = n;
-
 
 while (n != 0)
 {
@@ -33,7 +19,6 @@ while (n != 0)
 
     Console.WriteLine("введите число: ");
     n = Convert.ToInt32(Console.ReadLine());
-
 }
-if (max1 == max2) Console.WriteLine($"второй максимум = первому {max2}");
+if (max1 == max2) Console.WriteLine($"нет второго максимума, первый максимум = {max1}");
 else Console.WriteLine($"второй максимум = {max2}");
