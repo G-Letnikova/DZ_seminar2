6e1a786b081d4d1e408060660486d5f8613eafaf
GLetnikova <g.letnikova@gmail.com>
2022-12-03 09:39:20 +0300

    ДЗ_работа над ошибками

--- Dop/Program.cs
+++ Dop/Program.cs
@@ -1,22 +1,39 @@
-﻿// Второй максимум
+﻿// Определить второй максимум. 
+// Входные данные: гарантируется, что будет 0, и перед ним как минимум 2 числа
 //
 
 Console.Clear();
+
+/*
+Console.WriteLine("введите число: ");
+int max1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число: ");
+int max2 = Convert.ToInt32(Console.ReadLine());
+
+if (max2 > max1) { int k = max1; max1 = max2; max2 = k; }
+
+int n = max1;
+*/
 
 
 Console.WriteLine("введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 int max1 = n;
 int max2 = n;
+
+
 while (n != 0)
 {
     if (n > max1) 
-    { 
-        max2 = max1;
-        max1 = n;
-    }
-        Console.WriteLine("введите число: ");
-        n = Convert.ToInt32(Console.ReadLine());
+        { 
+            max2 = max1;
+            max1 = n;
+        }
+        else if (max2 == max1)   max2 = n;
+
+    Console.WriteLine("введите число: ");
+    n = Convert.ToInt32(Console.ReadLine());
 
 }
-Console.WriteLine($"второй максимум = {max2}");
+if (max1 == max2) Console.WriteLine($"второй максимум = первому {max2}");
+else Console.WriteLine($"второй максимум = {max2}");
--- Task10/Program.cs
+++ Task10/Program.cs
@@ -5,5 +5,5 @@ Console.Clear();
 Console.WriteLine("введите трехзначное число: ");
 int N = Convert.ToInt32(Console.ReadLine());
 if (N<0) N = (-1)*N;
-if  (N>=99 && N<1000) Console.WriteLine($"{(N/10)%10}");
+if  (N>99 && N<1000) Console.WriteLine($"{(N/10)%10}");
 else Console.WriteLine($"ошибка! число не трехзначное");
